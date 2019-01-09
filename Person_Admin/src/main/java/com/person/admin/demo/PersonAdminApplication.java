645da707bac66adaa9e85be69edd1d6f91e03b27
koteshwara_sastry <koteshwara_sastry@II39-14NTQQ2.mhf.mhc>
2019-01-09 07:21:34 +0530

    Eureka server

--- Person_Admin/src/main/java/com/person/admin/demo/PersonAdminApplication.java
+++ Person_Admin/src/main/java/com/person/admin/demo/PersonAdminApplication.java
@@ -3,10 +3,12 @@ package com.person.admin.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan({"com.person.admin.demo.controller","com.person.admin.demo.service"})
