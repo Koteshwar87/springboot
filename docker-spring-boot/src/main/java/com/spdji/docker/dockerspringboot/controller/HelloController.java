87105a2d9557cb439035498033d6d13a22490682
koteshwara_sastry <koteshwara_sastry@II39-14NTQQ2.mhf.mhc>
2019-08-18 17:35:32 +0530

    spring boot docker tutorial

---
+++ docker-spring-boot/src/main/java/com/spdji/docker/dockerspringboot/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.spdji.docker.dockerspringboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/docker/hello")
+public class HelloController {
+	
+	@GetMapping()
+	public String hello() {
+		return "hello from docker app";
+	}
+}
